45d65e9dfb2af6c28df86c3dfb79e4bba9cac9a1
jaycalixto <jaymecalixto@gmail.com>
2017-08-08 21:37:39 -0300

    changed resolution strategy

--- utils/Window.hx
+++ utils/Window.hx
@@ -8,11 +8,32 @@ class Window{
 	public var windowWidth:Int;
 	public var windowHeight:Int;
 
+	private var forcedResolution = false;
+
 	public function new(){
 		resizeCanvas();
 	}
 
-	public function resizeCanvas(){
+	public function forceIntegerResolution(baseWidth:Int, baseHeight:Int) {
+
+		if (windowWidth == 0){
+			throw "call resizeCanvas() first";
+		}
+
+		var coefWidth = windowWidth / baseWidth;
+		var coefHeight = windowHeight / baseHeight;
+
+		var coef = Std.int(Math.min(coefWidth, coefHeight));
+
+		windowWidth = baseWidth * coef;
+		windowHeight = baseHeight * coef;
+
+		forcedResolution = true;
+
+		resizeCanvas();
+	}
+
+	public function resizeCanvas() {
 		#if js
 
 		var c:CanvasElement = cast Browser.document.getElementById("khanvas");
@@ -22,16 +43,23 @@ class Window{
 		c.style.background = "#FF0000";
 		c.style.position = "absolute";
 
-		windowWidth = Std.int(Browser.window.innerWidth * 0.95);
-		windowHeight = Std.int(Browser.window.innerHeight * 0.95);
+		var fullResWidth = Std.int(Browser.window.innerWidth * 0.95);
+		var fullResHeight = Std.int(Browser.window.innerHeight * 0.95);
 
-		c.width = windowWidth;
-		c.height = windowHeight;
+		if (!forcedResolution) {
+			windowWidth = fullResWidth;
+			windowHeight = fullResHeight;
+		}
+
+		c.width = fullResWidth;
+		c.height = fullResHeight;
 
 		#else
 
-		windowWidth = kha.System.windowWidth;
-		windowHeight = kha.System.windowHeight;
+		if (!forcedResolution) {
+			windowWidth = kha.System.windowWidth;
+			windowHeight = kha.System.windowHeight;
+		}
 
 		#end
 	}
