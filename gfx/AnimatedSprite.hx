a8ecb06cb3ce9c9131217cc83ec04366aa229555
Jayme <jaymecalixto@gmail.com>
2017-08-20 21:28:00 -0300

    animated sprite rework

--- gfx/AnimatedSprite.hx
+++ gfx/AnimatedSprite.hx
@@ -3,47 +3,65 @@ package mankha.gfx;
 import kha.graphics2.Graphics;
 import kha.Image;
 import kha.Color;
-import haxe.ds.Vector;
+import mankha.utils.Rectangle;
 
 class AnimatedSprite extends Sprite {
 
+	public var animationRate(default, default):Float;
 	public var frameW(default, null):Int;
 	public var frameH(default, null):Int;
 	private var frameIndex:Int;
-	private var frames:Vector<AnimationFrame>;
+	public var framesUp(default, default):Array<Rectangle>;
+	public var framesDown(default, default):Array<Rectangle>;
+	public var framesLeft(default, default):Array<Rectangle>;
+	public var framesRight(default, default):Array<Rectangle>;
 	private var lastTime:Float;
+	private var dir:Direction;
+	private var currentFrames:Array<Rectangle>;
 
-	public function new(img: Image, frameWidth:Int, frameHeight:Int) {
+	public function new(img: Image, width:Int, height:Int) {
 		super(img);
-		frameW = frameWidth;
-		frameH = frameHeight;
-		frameIndex = 0;
-
-		var totalColumns = Std.int(img.width / frameW);
-		var totalRows = Std.int(img.height / frameH);
-
-		frames = new Vector<AnimationFrame>(totalColumns * totalRows);
-
-		for(i in 0...totalRows) {
-			for(j in 0...totalColumns) {
-				var anim = new AnimationFrame((totalColumns * i) + j, j * frameW, i * frameH);
-
-				frames[anim.frameIndex] = anim;
-			}
-		}
+		dir = Direction.DOWN;
+		animationRate = 0.2;
+		this.frameW = width;
+		this.frameH = height;
+		this.frameIndex = 0;
 	}
 
 	public override function draw(g:Graphics){
 		if(isVisible){
 			g.color = color;
-			g.drawSubImage(image, position.x, position.y, frames[frameIndex].sourceX, frames[frameIndex].sourceY, frameW, frameH);
+
+			switch (dir) {
+				case DOWN: currentFrames = framesDown;
+				case UP: currentFrames = framesUp;
+				case LEFT: currentFrames = framesLeft;
+				case RIGHT: currentFrames = framesRight;
+				default: trace("direction not found " + dir);
+			}
+
+			g.drawSubImage(image, position.x, position.y, 
+				currentFrames[frameIndex].x, currentFrames[frameIndex].y,
+				currentFrames[frameIndex].width, currentFrames[frameIndex].height);
+
 			g.color = Color.White;
 		}
-		if( (kha.Scheduler.time() - lastTime) > 0.2){
+
+		if ((kha.Scheduler.time() - lastTime) > animationRate) {
 			lastTime = kha.Scheduler.time();
 			frameIndex++;
-			frameIndex = frameIndex % 4;
-		}		
+			frameIndex = frameIndex % currentFrames.length;
+		}
 	}
 
+	public function changeDirection(dir:Direction){
+		this.dir = dir;
+	}
+}
+
+enum Direction {
+		UP;
+		DOWN;
+		LEFT;
+		RIGHT;
 }
